45831382fe0325b938674951cdde4242088d1601
andres.amdc@gmail.com <andres.amdc@gmail.com>
2019-12-18 19:20:48 -0600

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-PruebaSDK_Tests/Pods-PruebaSDK_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PruebaSDK_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_PruebaSDK_Tests
+@end
---
+++ Example/Pods/Target Support Files/PruebaSDK/PruebaSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PruebaSDK : NSObject
+@end
+@implementation PodsDummy_PruebaSDK
+@end
